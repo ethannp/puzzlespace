bf56660e697a4999f4391300c99ed5dd0af54c98
Vinhan Truong <v.truong1478@gmail.com>
2020-11-20 13:34:16 -0500

    fixed star background
    star background shows up but is bad quality

--- app.js
+++ app.js
@@ -34,6 +34,25 @@ let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
 */
 
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var canvas = document.getElementById("starfield");
+var context = canvas.getContext("2d");
+var stars = 500;
+var colorrange = [0, 60, 240];
+for (var i = 0; i < stars; i++) {
+  var x = Math.random() * canvas.offsetWidth;
+  var y = Math.random() * canvas.offsetHeight;
+  var radius = Math.random() * 1.2;
+  var hue = colorrange[getRandom(0, colorrange.length - 1)];
+  var sat = getRandom(50, 100);
+  context.beginPath();
+  context.arc(x, y, radius, 0, 360);
+  context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
+  context.fill();
+}
+
 var puzzles = [];
 var answers = [];
 var number;
@@ -162,23 +181,4 @@ function clearStorage() {
   window.localStorage.removeItem("answers");
   answers = [];
   console.log("clear");
-}
-
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-var canvas = document.getElementById("starfield"),
-  context = canvas.getContext("2d"),
-  stars = 500,
-  colorrange = [0, 60, 240];
-for (var i = 0; i < stars; i++) {
-  var x = Math.random() * canvas.offsetWidth;
-  (y = Math.random() * canvas.offsetHeight),
-    (radius = Math.random() * 1.2),
-    (hue = colorrange[getRandom(0, colorrange.length - 1)]),
-    (sat = getRandom(50, 100));
-  context.beginPath();
-  context.arc(x, y, radius, 0, 360);
-  context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
-  context.fill();
 }
