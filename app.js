09e482b9153efc85201483647a0d3f19951bab06
Vinhan Truong <v.truong1478@gmail.com>
2020-11-21 00:40:53 -0500

    removed star canvas code

--- app.js
+++ app.js
@@ -45,31 +45,9 @@ let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
 */
 
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-var canvas = document.getElementById("starfield");
-if (canvas) { // check if canvas exists
-  var context = canvas.getContext("2d");
-  var stars = 500;
-  var colorrange = [0, 60, 240];
-  for (var i = 0; i < stars; i++) {
-    var x = Math.random() * canvas.offsetWidth;
-    var y = Math.random() * canvas.offsetHeight;
-    var radius = Math.random() * 1.2;
-    var hue = colorrange[getRandom(0, colorrange.length - 1)];
-    var sat = getRandom(50, 100);
-    context.beginPath();
-    context.arc(x, y, radius, 0, 360);
-    context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
-    context.fill();
-  }
-}
-
 var puzzles = [];
 var answers = [];
 var number;
-
 
 //deleted
 
