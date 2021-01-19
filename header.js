1b3b860fee411d6e87b54210253c18d4787628a1
Vinhan Truong <v.truong1478@gmail.com>
2021-01-19 17:26:45 -0500

    navbar in header.html

---
+++ header.js
@@ -0,0 +1,48 @@
+"use strict";
+
+function loadNavbar() {
+  const navul = document.getElementById("menu-ul");
+
+  const logo = document.createElement("a");
+  logo.className = "puzzlespace-text";
+  logo.href = "index.html";
+  const img = document.createElement("img");
+  img.class = "logo-image";
+  img.src = "white-puzzle-piece.svg";
+  logo.appendChild(img); // im pretty sure this is the way to do it. if it doesnt work lmk
+  navul.appendChild(logo); // adds logo element to navul
+
+  //Home tab
+  const li1 = document.createElement("li");
+  const link1 = document.createElement("a");
+  link1.href = "index.html";
+  link1.innerHTML = "Home";
+  li1.appendChild(link1);
+  navul.appendChild(li1);
+
+  //Puzzles tab
+  const li2 = document.createElement("li");
+  const link2 = document.createElement("a");
+  link2.href = "puzzles.html";
+  link2.innerHTML = "Puzzles";
+  li2.appendChild(link2);
+  navul.appendChild(li2);
+
+  //Resources tab
+  const li3 = document.createElement("li");
+  const link3 = document.createElement("a");
+  link3.href = "resources.html";
+  link3.innerHTML = "Resources";
+  li3.appendChild(link3);
+  navul.appendChild(li3);
+
+  //Search tab
+  const li4 = document.createElement("li");
+  const link4 = document.createElement("a");
+  link4.href = "search.html";
+  link4.innerHTML = "Search";
+  li4.appendChild(link4);
+  navul.appendChild(li4);
+}
+
+document.body.onload = loadNavbar;
