2c31e75793d90c2b534b1af7d03e72c319c6b9ff
Vinhan Truong <v.truong1478@gmail.com>
2021-03-17 12:22:54 -0400

    put star canvas back

--- public/static/js/stars.js
+++ public/static/js/stars.js
@@ -1,117 +1,79 @@
 "use strict";
 
-// // DOM selectors
-// const stars = document.getElementById("stars");
-// const starsCtx = stars.getContext("2d");
-// const output = document.querySelector("#speed");
+// DOM selectors
+const stars = document.getElementById("stars");
+const starsCtx = stars.getContext("2d");
+const output = document.querySelector("#speed");
 
-// // global variables
-// let screen,
-//   starsElements,
-//   starsParams = { speed: 0.2, number: 300, extinction: 4 };
+// global variables
+let screen,
+  starsElements,
+  starsParams = { speed: 0.2, number: 300, extinction: 4 };
 
-// // run stars
-// setupStars();
-// updateStars();
+// run stars
+setupStars();
+updateStars();
 
-// // update stars on resize to keep them centered
-// window.onresize = function () {
-//   setupStars();
-// };
-
-// // star constructor
-// function Star() {
-//   this.x = Math.random() * stars.width;
-//   this.y = Math.random() * stars.height;
-//   this.z = Math.random() * stars.width;
-
-//   this.move = function () {
-//     this.z -= starsParams.speed;
-//     if (this.z <= 0) {
-//       this.z = stars.width;
-//     }
-//   };
-
-//   this.show = function () {
-//     let x, y, rad, opacity;
-//     x = (this.x - screen.c[0]) * (stars.width / this.z);
-//     x = x + screen.c[0];
-//     y = (this.y - screen.c[1]) * (stars.width / this.z);
-//     y = y + screen.c[1];
-//     rad = stars.width / this.z;
-//     opacity =
-//       rad > starsParams.extinction
-//         ? 1.5 * (2 - rad / starsParams.extinction)
-//         : 1;
-
-//     starsCtx.beginPath();
-//     starsCtx.fillStyle = "rgba(255, 255, 255, " + opacity + ")"; //star color
-//     starsCtx.arc(x, y, rad, 0, Math.PI * 2);
-//     starsCtx.fill();
-//   };
-// }
-
-// // setup <canvas>, create all the starts
-// function setupStars() {
-//   screen = {
-//     w: window.innerWidth,
-//     h: window.innerHeight,
-//     c: [window.innerWidth * 0.5, window.innerHeight * 0.5],
-//   };
-//   window.cancelAnimationFrame(updateStars);
-//   stars.width = screen.w;
-//   stars.height = screen.h;
-//   starsElements = [];
-//   for (let i = 0; i < starsParams.number; i++) {
-//     starsElements[i] = new Star();
-//   }
-// }
-
-// // redraw the frame
-// function updateStars() {
-//   starsCtx.fillStyle = "rgb(0,0,0)"; //space background color
-//   starsCtx.fillRect(0, 0, stars.width, stars.height);
-//   starsElements.forEach(function (s) {
-//     s.show();
-//     s.move();
-//   });
-//   window.requestAnimationFrame(updateStars);
-// }
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-var myImg = new Image();
-var myImgPos = {
-  x: 250,
-  y: 125,
-  width: 50,
-  height: 25,
+// update stars on resize to keep them centered
+window.onresize = function () {
+  setupStars();
 };
 
-function draw() {
-  myImg.onload = function () {
-    ctx.drawImage(
-      myImg,
-      myImgPos.x,
-      myImgPos.y,
-      myImgPos.width,
-      myImgPos.height
-    );
+// star constructor
+function Star() {
+  this.x = Math.random() * stars.width;
+  this.y = Math.random() * stars.height;
+  this.z = Math.random() * stars.width;
+
+  this.move = function () {
+    this.z -= starsParams.speed;
+    if (this.z <= 0) {
+      this.z = stars.width;
+    }
   };
 
-  myImg.src = "static/image/PuzzleGuideByHunt20.png";
+  this.show = function () {
+    let x, y, rad, opacity;
+    x = (this.x - screen.c[0]) * (stars.width / this.z);
+    x = x + screen.c[0];
+    y = (this.y - screen.c[1]) * (stars.width / this.z);
+    y = y + screen.c[1];
+    rad = stars.width / this.z;
+    opacity =
+      rad > starsParams.extinction
+        ? 1.5 * (2 - rad / starsParams.extinction)
+        : 1;
+
+    starsCtx.beginPath();
+    starsCtx.fillStyle = "rgba(255, 255, 255, " + opacity + ")"; //star color
+    starsCtx.arc(x, y, rad, 0, Math.PI * 2);
+    starsCtx.fill();
+  };
 }
 
-function moveMyImg() {
-  ctx.clearRect(
-    myImgPos.x,
-    myImgPos.y,
-    myImgPos.x + myImgPos.width,
-    myImgPos.y + myImgPos.height
-  );
-  myImgPos.x -= 5;
+// setup <canvas>, create all the starts
+function setupStars() {
+  screen = {
+    w: window.innerWidth,
+    h: window.innerHeight,
+    c: [window.innerWidth * 0.5, window.innerHeight * 0.5],
+  };
+  window.cancelAnimationFrame(updateStars);
+  stars.width = screen.w;
+  stars.height = screen.h;
+  starsElements = [];
+  for (let i = 0; i < starsParams.number; i++) {
+    starsElements[i] = new Star();
+  }
 }
 
-setInterval(draw, 50);
-setInterval(moveMyImg, 50);
+// redraw the frame
+function updateStars() {
+  starsCtx.fillStyle = "rgb(0,0,0)"; //space background color
+  starsCtx.fillRect(0, 0, stars.width, stars.height);
+  starsElements.forEach(function (s) {
+    s.show();
+    s.move();
+  });
+  window.requestAnimationFrame(updateStars);
+}
